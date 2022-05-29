b41541d8bb0cec6e8a39b39211f07dcfc6091ad5
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-29 22:42:22 +0800

    Delete scripts directory

--- scripts/train_chunker_v2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh -x
-
-if [ "$exe_bin" = "" ];
-then echo "exe_bin not set";
-     exit;
-fi
-
-cd ../data
-$exe_bin/chunkgen  -c $exe_bin/../share/frog/nld//frog.cfg chunker.train -O chunkgen/
--- scripts/train_new_ner.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh -x
-
-if [ "$exe_bin" = "" ];
-then echo "exe_bin not set";
-     exit;
-fi
-
-$exe_bin/nergen -O NERDATA -c $exe_bin/../share/frog/nld/frog.cfg --gazeteer=$exe_bin/../share/frog/nld/ners.known NERDATA/ner.train
